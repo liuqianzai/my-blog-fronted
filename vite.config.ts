afaf27caa79dce716cd934880347b0d4fb70d646
LiuYang <2823545005@qq.com>
2026-05-21 16:06:33 +0800

    feat: add Gzip compression and code splitting config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 10240,
+      deleteOriginFile: false,
+    })
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'element-plus': ['element-plus'],
+          'markdown': ['markdown-it', 'highlight.js'],
+          'editor': ['md-editor-v3'],
+        }
+      }
+    }
+  }
 })
